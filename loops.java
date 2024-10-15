298dc29c4b3ba6fdf5323fb92dc5381bc5b93ab9
Tester Shailesh <you@example.com>
2024-10-15 18:36:03 +0530

    removed file

--- loops.java
+++
@@ -1,9 +0,0 @@
-package javaprograms;
-
-public class loops {
-    public static void main(String[] args) {
-        for(int i=0;i<10;i++){
-            System.out.println("Hello My Name is Shailesh");
-        }
-    }
-}
